4080efe7130443f9286cd43709f36967296963d9
Simon <00pflaume@gmail.com>
2018-06-05 11:46:39 +0200

    Sends a messages to the spyer on message

--- src/main/java/de/simonsator/partyandfriends/logtool/logger/FriendLogger.java
+++ src/main/java/de/simonsator/partyandfriends/logtool/logger/FriendLogger.java
@@ -3,8 +3,10 @@ package de.simonsator.partyandfriends.logtool.logger;
 import de.simonsator.partyandfriends.api.pafplayers.OnlinePAFPlayer;
 import de.simonsator.partyandfriends.api.pafplayers.PAFPlayer;
 import de.simonsator.partyandfriends.api.pafplayers.PAFPlayerManager;
+import de.simonsator.partyandfriends.friends.commands.Friends;
 import de.simonsator.partyandfriends.logtool.FriendSpyContainer;
 import de.simonsator.partyandfriends.logtool.LogToolMain;
+import de.simonsator.partyandfriends.main.Main;
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -13,6 +15,9 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+
+import static de.simonsator.partyandfriends.utilities.PatterCollection.*;
 
 /**
  * @author simonbrungs
@@ -31,7 +36,22 @@ public class FriendLogger extends Logger implements Listener {
 
 	@Override
 	public void writeln(OnlinePAFPlayer pSender, Object pReceiver, String pMessage) {
+		PAFPlayer receiverPAFPlayer = ((PAFPlayer) pReceiver);
 		cache.add(pSender.getName() + "->" + ((PAFPlayer) pReceiver).getName() + ":" + pMessage);
+		for (FriendSpyContainer spyContainer : spyList) {
+			if (spyContainer.SPYED_ON.equals(pSender) || spyContainer.SPYED_ON.equals(pReceiver)) {
+				String message = (Friends.getInstance().getPrefix() + CONTENT_PATTERN.matcher(PLAYER_PATTERN.matcher(SENDER_NAME_PATTERN.matcher(Main.getInstance()
+						.getMessages().getString("Friends.Command.MSG.SentMessage")).replaceAll(Matcher.quoteReplacement(pSender.getDisplayName()))).replaceAll(Matcher.quoteReplacement(receiverPAFPlayer.getDisplayName()))).replaceAll(Matcher.quoteReplacement(pMessage.replace(" ", Main.getInstance().getMessages().getString("Friends.Command.MSG.ColorOfMessage")))));
+				spyContainer.SPYER.sendMessage(message);
+			}
+		}
+	}
+
+	public boolean addSpy(FriendSpyContainer pSpyContainer) {
+		if (spyList.contains(pSpyContainer))
+			return false;
+		spyList.add(pSpyContainer);
+		return true;
 	}
 
 	public boolean removeFromSpyList(OnlinePAFPlayer pSpyer, PAFPlayer pSpyedOn) {
